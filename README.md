196daeab2bacd16b627d2b955bfa9857589f0824
Rajkumar S <rajkumaar2304@icloud.com>
2020-06-11 12:34:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# elementary-bot
+
+A simple retweet bot to stay updated on what people are tweeting about [@elementary](https://twitter.com/elementary)
---
+++ bot.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'dotenv/load'
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["ACCESS_TOKEN"]
+  config.access_token_secret = ENV["ACCESS_SECRET"]
+end
+
+numberOfTweets = 50
+
+client.search("@elementary", result_type: "recent").take(numberOfTweets).collect do |tweet|
+  client.retweet tweet
+  sleep(1)
+end
